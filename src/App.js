e5ff6fc6c249db78cb9d97414e721f3430fb52a9
TravisPRyan <TravisPRyan@protonmail.com>
2019-10-30 14:30:54 -0400

    v2

--- src/App.js
+++ src/App.js
@@ -1,16 +1,19 @@
 import React, { Component } from 'react';
+import Navbar from './components/layout/Navbar';
+import Users from './components/users/Users';
 import './App.css';
+
 
 class App extends Component {
   render() {
-    const name = 'John Doe';
-    const foo = () => 'Bar';
-    const loading = true;
-
     return (
-      <div className="App">
-        <h1>My App</h1>
-        {loading ? <h4>Loading...</h4> : <h1>Hello {name}!</h1>}
+      <div className='App'>
+        <Navbar title='Hub Crawler' icon='fab fa-github' /> 
+        <div className='container'>
+          <Users />
+        </div>
+
+        
         
       </div>
     );

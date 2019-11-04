d4bdd973ecc149fdcf1c473da730e30e2a30fd1f
TravisPRyan <TravisPRyan@protonmail.com>
2019-11-04 16:09:13 -0500

    v8 Routes

--- src/components/layout/Navbar.js
+++ src/components/layout/Navbar.js
@@ -1,13 +1,22 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import {Link} from 'react-router-dom';
 
-
+// Use Link (react-router-dom) instaed of <a> to keep original state intact
 const Navbar = ({icon, title}) => {
     return (
         <nav className='navbar bg-primary'>
             <h1>
                 <i className={icon} /> {title}
             </h1>
+            <ul>
+                <li> 
+                    <Link to="/">Home</Link>
+                </li>
+                <li>
+                    <Link to="/about">About</Link>
+                </li>
+            </ul>
         </nav>
     );
 };
---
+++ src/components/pages/About.js
@@ -0,0 +1,13 @@
+import React, {Fragment} from 'react';
+
+const About = () => {
+    return (
+        <Fragment>
+            <h1>About this app</h1>
+            <p>App to search GitHub users</p>
+            <p>V 1.0.0</p>
+        </Fragment>
+    )
+}
+
+export default About

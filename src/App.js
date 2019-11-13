f62772bd22b1b851c51e09d465fd7043402e1718
TravisPRyan <TravisPRyan@protonmail.com>
2019-11-13 17:08:20 -0500

    v19 Alert Migrate

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Fragment, useState } from 'react';
+import React, {Fragment} from 'react';
 import {BrowserRouter as Router, Switch, Route} from 'react-router-dom';
 import Navbar from './components/layout/Navbar';
 import Users from './components/users/Users';
@@ -7,46 +7,37 @@ import Search from './components/users/Search';
 import Alert from './components/layout/Alert';
 import About from './components/pages/About';
 import GithubState from './context/github/GithubState';
+import AlertState from './context/alert/AlertState';
+
+
 import './App.css';
 
 
 const App = () => {
-  const [alert, setAlert] = useState(null);
- 
-  
-  // Set Temp Alert
-  const dispAlert = (message, type) => {
-    setAlert({message, type})
-    setTimeout(() => (null), 2000);
-  };
-
 
   return (
     <GithubState>
-    <Router>
-    <div className='App'>
-      <Navbar title='Hub Crawler' icon='fab fa-github' /> 
-      <div className='container'>
-        <Alert alert={alert} />
-        <Switch>
-          <Route exact path='/' render={props =>(
-            <Fragment>
-              <Search 
-                
+      <AlertState>
+        <Router>
+        <div className='App'>
+          <Navbar title='Hub Crawler' icon='fab fa-github' /> 
+          <div className='container'>
+            <Alert />
+            <Switch>
+              <Route exact path='/' render={props =>(
+                <Fragment>
+                  <Search/>
+                  <Users/>
+                </Fragment>
+              )}/>
+              <Route exact path='/about' component={About}/>
+              <Route exact path='/user/:login' component={User}/>
+            </Switch>
 
-                setAlert={dispAlert}
-              />
-              <Users/>
-
-            </Fragment>
-          )}/>
-          <Route exact path='/about' component={About}/>
-          <Route exact path='/user/:login' component={User}/>
-        </Switch>
-
-      </div>
-    </div>
-    </Router>
+          </div>
+        </div>
+        </Router>
+      </AlertState>
     </GithubState>
   );
 };
